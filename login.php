d87744b3d62efcaab6d1407140dfec4a5c050313
Russ Meili <rmeili69@netscape.net>
2017-01-29 13:43:35 -0800

    Update Login and Registration

--- login.php
+++ login.php
@@ -1,34 +1,57 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: JoshC
- * Date: 1/22/2017
- * Time: 4:51 PM
+ * User: russm
+ * Date: 1/29/2017
+ * Time: 10:36 AM
  */
+
+ob_start();
+include "includes/functions.php";
 include "includes/header.php";
 
-$form = <<<LOGIN
-<form method="POST" action="/index.php">
-        <label for="username">Username:</label>
-        <input type="text" name="username" placeholder="username"/><br />
-        <label for="password">Password:</label>
-        <input type="text" name="password" placeholder="password" />
-        &nbsp;&nbsp;&nbsp;
-        <input type="submit" name="submit" value="Sign in"/>
+$db = db_connect();
+
+$firstname = mysqli_real_escape_string( $db, $_POST["firstname"]);
+$lastname = mysqli_real_escape_string( $db, $_POST["lastname"]);
+$username = mysqli_real_escape_string( $db, $_POST["username"]);
+$password = mysqli_real_escape_string( $db, $_POST["password"]);
+$verifypassword = mysqli_real_escape_string( $db, $_POST["verifypassword"]);
+$email = mysqli_real_escape_string( $db, $_POST["email"]);
+$advanced_user = mysqli_real_escape_string( $db, $_POST["advanced_user"]);
+$submit = mysqli_real_escape_string( $db, $_POST["submit"]);
+
+if ($submit){
+    $sql = "SELECT user_name from users where user_name='$username'";
+    $result = $db->query( $sql );
+    if( $result && $result->num_rows){
+        list( $username, $password_hash ) = $result->fetch_row();
+        if(password_verify( $password, $password_hash )){
+            $_SESSION['username'] = $username;
+            ob_clean();
+            header("Location: /");
+        }
+    } else {
+        $error_msg = "Unknown credentials - Please try again";
+    }
+}
+
+$login_form = <<<END_OF_FORM
+    <p>$error_msg<p>
+    <form method="POST" action="/login.php">
+        <label for"username">Username: </label><br/>
+        <input type="text" name="username" value="$username"/><br/>
+        <label for"password">Password: </label><br/>
+        <input type="password" name="password" value=""/><br/>
+        <input type="submit" name="submit" value="Submit"/><br/>
     </form><br/>
-LOGIN;
+END_OF_FORM;
+
 echo "<div class=\"login_box\">";
-echo $form;
+echo $login_form;
 echo "<a href=\"registration.php\">New User? Register here</a>";
 echo "</div>";
-?>
 
-<?php
-/**
- * Created by PhpStorm.
- * User: JoshC
- * Date: 1/22/2017
- * Time: 4:51 PM
- */
 include "includes/footer.php";
+ob_end_flush();
 ?>
--- registration.php
+++ registration.php
@@ -7,68 +7,78 @@
  */
 ob_start();
 include "includes/functions.php";
+include "includes/header.php";
 
 $db = db_connect();
 
-//
-//$username = mysqli_real_escape_string( $db, $_POST["username"]);
-//$password = mysqli_real_escape_string( $db, $_POST["password"]);
-//$email = mysqli_real_escape_string( $db, $_POST["email"]);
-//$submit = mysqli_real_escape_string( $db, $_POST["submit"]);
-//
-//$encrypted_password = password_hash( $password, PASSWORD_DEFAULT );
-//
-//$registration_error = "";
-//if( $submit ){
-//    if( empty( $username ) )
-//        $registration_error .= "Must supply username<br/>";
-//    if( empty( $email ) )
-//        $registration_error .= "Must supply email<br/>";
-//    if( empty( $password ) )
-//        $registration_error .= "Must supply password<br/>";
-//
-//    // Check that this is a unique user - by checking if they exist
-//    $sql = "SELECT username from users where username='$username'";
-//    $result = $db->query( $sql );
-//    if( $result->num_rows > 0){
-//        // Name already exists
-//        $registration_error .= "<p>Username already exists...try again!</p>";
-//    } else {
-//        if( empty($registration_error)){
-//            // OK - insert the user into the db
-//            $sql = "INSERT INTO users (id, username, password, email)
-//                                values(null, '$username', '$encrypted_password', '$email')";
-//            $result = $db->query( $sql );
-//            // Look at $result errors and display if there are some
-////            if( ! $result ){
-////                $registration_error .= "(" . $db->connect_errno . ")" .
-////            $db->connect_error;
-////            }
-//            ob_clean();
-//            header( "Location: /");
-//        }
-//    }
-//    //create a new user
-//  $registration_error  //redirect if ok
-//}
+$firstname = mysqli_real_escape_string( $db, $_POST["firstname"]);
+$lastname = mysqli_real_escape_string( $db, $_POST["lastname"]);
+$username = mysqli_real_escape_string( $db, $_POST["username"]);
+$password = mysqli_real_escape_string( $db, $_POST["password"]);
+$verifypassword = mysqli_real_escape_string( $db, $_POST["verifypassword"]);
+$email = mysqli_real_escape_string( $db, $_POST["email"]);
+$advanced_user = mysqli_real_escape_string( $db, $_POST["advanced_user"]);
+$submit = mysqli_real_escape_string( $db, $_POST["submit"]);
+
+$encrypted_password = password_hash( $password, PASSWORD_DEFAULT );
+
+$registration_error = "";
+if( $submit ){
+    if( empty( $username ) )
+        $registration_error .= "Must supply username<br/>";
+    if( empty( $email ) )
+        $registration_error .= "Must supply email<br/>";
+    if( empty( $password ) )
+        $registration_error .= "Must supply password<br/>";
+    if( $password != $verifypassword )
+        $registration_error .= "Passwords must match<br/>";
+
+    // Check that this is a unique user - by checking if they exist
+    $sql = "SELECT user_name from users where user_name='$username'";
+    $result = $db->query( $sql );
+    if( $result->num_rows > 0){
+        // Name already exists
+        $registration_error .= "<p>Username already exists...try again!</p>";
+    } else {
+        if( empty($registration_error)){
+            // OK - insert the user into the db
+            $sql = "INSERT INTO users (user_id, f_name, l_name, user_name, password, email, advanced)
+                                values(null, '$firstname', '$lastname', '$username', '$encrypted_password', '$email', '$advanced_user')";
+            $result = $db->query( $sql );
+            // Look at $result errors and display if there are some
+//            if( ! $result ){
+//                $registration_error .= "(" . $db->connect_errno . ")" .
+//            $db->connect_error;
+//            }
+            ob_clean();
+            header( "Location: /login.php");
+        }
+    }
+    //create a new user
+    $registration_error;  //redirect if ok
+}
 
 $form = <<<END_OF_FORM
     
     <form method="POST" action="/register.php">
         <label for"firstname">First Name: </label><br/>
-        <input type="text" name="firstname" value="firstname"/><br/>
+        <input type="text" name="firstname" value="$firstname"/><br/>
         <label for"lastname">Last Name: </label><br/>
-        <input type="text" name="lastname" value="lastname"/><br/>
+        <input type="text" name="lastname" value="$lastname"/><br/>
         <label for"username">Username: </label><br/>
-        <input type="text" name="username" value="username"/><br/>
+        <input type="text" name="username" value="$username"/><br/>
         <label for"password">Password: </label><br/>
         <input type="password" name="password" value=""/><br/>
+        <label for"verifypassword">Verify Password: </label><br/>
+        <input type="password" name="verifypassword" value=""/><br/>
         <label for"email">Email: </label><br/>
-        <input type="email" name="email" value="email"/><br/>
+        <input type="checkbox" name="advanced_user" id="advanced_user" value="yes" $advanced_user>
+        <input type="email" name="email" value="$email"/><br/>
         <input type="submit" name="submit" value="Submit"/><br/>
     </form><br/>
 END_OF_FORM;
 
 echo $form;
+include "include/footer";
 ob_end_flush()
 ?>
