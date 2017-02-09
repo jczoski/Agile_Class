039270a87e6058c1d46c53fafd4f14dda6cd68b4
Josh Czoski <josh.czoski@gmail.com>
2017-02-08 21:34:32 -0800

    I made the posts editable and made replies save and post.

--- discussion.php
+++ discussion.php
@@ -11,38 +11,65 @@ include "includes/functions.php";
 $db = db_connect();
 
 $topic = mysqli_real_escape_string($db, $_GET["topic_name"]);
+if(!$topic) {
+    $topic = mysqli_real_escape_string($db, $_POST["reply_topic"]);
+}
+$copy_topic = $topic;
 $author = mysqli_real_escape_string($db, $_GET["author"]);
 $category = mysqli_real_escape_string($db, $_GET["category"]);
+if(!$category){
+$category =mysqli_real_escape_string($db, $_POST["reply_category"]);
+}
+$copy_category=$category;
+$submit = $_POST["submit"];
+$reply_text = mysqli_real_escape_string($db,$_POST["reply"]);
+$reply_user = mysqli_real_escape_string($db, $_POST["reply_user"]);
+
+if($submit){
+    $sql = "INSERT INTO posts(id,author,topic_name,category,post_date,modify_date,post_text)
+VALUES(null,'$reply_user','$topic','$category',NOW(),NOW(),'$reply_text')";
+    $result = $db->query($sql);
+
+}
+
 
 $sql = "SELECT * from posts where topic_name='$topic' and category='$category'";
+
 $result = $db->query($sql);
 echo "<h2>$topic</h2>";
 
 echo "<table class=\"table-style\"><th>Author</th><th>Topic: $topic</th>";
 while (list($id, $author, $topic, $category, $post_date, $modify_date, $post_text) = $result->fetch_row()) {
 //$post_date = date("D",$post_date) . date("M",$post_date). date("j",$post_date).date("o",$post_date);
+    $editable = "&nbsp;";
+    if ($author === $_SESSION["username"]) {
+        $editable = "<a href='edit_post.php?id=$id'>Edit post</a>";
+    }
     $posts = <<<POSTS
 
-<tr><td>$author</td><td>posted: $post_date<br />$post_text</td></tr>
+<tr><td>$author</td><td>posted: $post_date<br />$post_text</td><td>$editable</td></tr>
 
 POSTS;
+
+    echo $posts;
 }
-echo $posts;
 echo "</table>";
 if (isset($_SESSION["username"])) {
-//echo "<a href=\"reply.php\"><h2>Reply</h2></a>";
-
-    ?>
-
+    $user_name = $_SESSION["username"];
+    $return_address = $_SERVER['PHP_SELF'];
+$reply_form=<<<REPLY_FORM
     <div>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <form action="$return_address" method="post">
             <label for="reply">Reply:</label><br/>
             <textarea name="reply" rows="15" cols="55"></textarea><br/>
+            <input type="hidden" name="reply_user" value="$user_name">
+            <input type="hidden" name="reply_topic" value="$copy_topic" >
+            <input type="hidden" name="reply_category" value="$copy_category" >
             <input type="submit" name="submit" value="reply"/>
         </form>
     </div>
-
-    <?php
+REPLY_FORM;
+echo $reply_form;
 }
 include "includes/footer.php";
 ?>
---
+++ edit_post.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoshC
+ * Date: 2/8/2017
+ * Time: 7:01 PM
+ */
+ob_start();
+include "includes/functions.php";
+include "includes/header.php";
+
+$db = db_connect();
+$post_id = mysqli_real_escape_string($db,$_GET["id"]);
+$back_link= $_SERVER['HTTP_REFERER'];
+$submit = $_POST["submit"];
+$modified_message = mysqli_real_escape_string($db,$_POST["message"]);
+$id = mysqli_real_escape_string($db, $_POST["same_id"]);
+$topic_return = mysqli_real_escape_string($db, $_POST["topic_return"]);
+if($submit){
+    $sql = "update posts set post_text='$modified_message', modify_date=NOW() where id=$id";
+    echo $sql;
+    $result = $db->query($sql);
+    ob_clean();
+    header("Location: $topic_return");
+}
+else {
+    $sql = "SELECT * from posts where id=$post_id";
+    list($id, $author, $topic, $category, $post_date, $modify_date, $post_text) = $db->query($sql)->fetch_row();
+}
+$post_topic = <<<TOPIC_FORM
+<h2>Edit Post - $topic</h2>
+<form action="edit_post.php" method="post">
+<label for="message">Message:</label><br />
+<textarea name="message" rows="25" cols="155">$post_text</textarea><br />
+<input type="hidden" name="same_id" value=$post_id>
+<input type="hidden" name="topic_return" value="$back_link">
+<input type="submit" name="submit" value="Edit Post" />
+</form>
+TOPIC_FORM;
+echo "<div class=\"post_message\">";
+echo $post_topic;
+
+echo "</div>";
+
+include "includes/footer.php";
+ob_end_flush();
+?>
