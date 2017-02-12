b5ae77eb8f6632850e6e485fe4e706b59d0d5db0
Russ Meili <rmeili69@netscape.net>
2017-02-12 15:16:40 -0800

    Centering tables, created footed, css tweaks

--- post_list.php
+++ post_list.php
@@ -9,16 +9,18 @@ include "includes/functions.php";
 include "includes/header.php";
 $db = db_connect();
 $forum_type = mysqli_real_escape_string($db,$_GET["forum_type"]);
-echo"<h2>$forum_type</h2>";
+
 if(isset($_SESSION["username"]))
 echo "<div class=\"forum_header\"> <a href=\"newtopic.php?forum_type=$forum_type\">Post New Topic</a> </div>";
 
 $sql = "SELECT author, topic_name,category,post_date,modify_date from posts where category='$forum_type' and opening_post=1";
 $result = $db->query($sql);
 //<td></td>
-echo "<table class=\"table-style\">";
+echo "<table class=\"table-style\">
+        <th colspan='4'>$forum_type</th>";
 while(list($author,$topic_name,$category,$post_date,$modify_date) = $result->fetch_row()){
     $post_list = <<<POST_LIST
+
 
 <tr><td><h3><a href="discussion.php?author=$author&topic_name=$topic_name&category=$category"> $topic_name</a></h3><br />started by $author</td><td></td><td></td><td></td></tr>
 
