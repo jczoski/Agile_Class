09c21e304721eef178e9c9dfd38f54651ff51b82
Russ Meili <rmeili69@netscape.net>
2017-02-20 18:06:02 -0800

    fixed database path on reg page

--- registration.php
+++ registration.php
@@ -46,9 +46,9 @@ if( $submit ){
             }
             // OK - insert the user into the db
             $sql = "INSERT INTO users (user_id, f_name, l_name, user_name, password, email, advanced, image, image_thumbnail, text_size)
-                                values(null, '$firstname', '$lastname', '$username', '$encrypted_password', '$email', $advanced_user_check,NULL,NULL,NULL)";
+                                values(null, '$firstname', '$lastname', '$username', '$encrypted_password', '$email', $advanced_user_check, null, null, 0)";
             $result = $db->query( $sql );
-            echo $sql;
+
             // Look at $result errors and display if there are some
 //            if( ! $result ){
 //                $registration_error .= "(" . $db->connect_errno . ")" .
