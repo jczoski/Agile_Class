be3100cd740d1390df959eff6caae5afc561a241
Russ Meili <rmeili69@netscape.net>
2017-02-27 10:55:47 -0800

    Created admin page

--- admin_page.php
+++ admin_page.php
@@ -18,15 +18,15 @@ $user_name = $_SESSION["username"];
 $sql = "Select * from users";
 $result =$db->query($sql);
 echo "<table class=\"table-style\"><th><h2>User</h2></th><th><h2>Email</h2></th><th><h2>Name</h2></th><th><h2>Delete</h2></th>";
-//
-//while(list($user_id, $f_name, $l_name, $user_name, $password, $email, $advanced,$image, $image_thumbnail, $text_size) = $db->query($sql)->fetch_row()){
-//
-//    $users = <<<USERS_FORM
-//<tr><td>$user_name</td><td>$email</td><td>$f_name $l_name</td><td><a href='Delete_user.php'>Delete User</a></td></tr>
-//
-//USERS_FORM;
-//    echo $users;
-//}
+
+while(list($user_id, $f_name, $l_name, $user_name, $password, $email, $advanced,$image, $image_thumbnail, $text_size) = $db->query($sql)->fetch_row()){
+
+    $users = <<<USERS_FORM
+<tr><td>$user_name</td><td>$email</td><td>$f_name $l_name</td><td><a href='Delete_user.php'>Delete User</a></td></tr>
+
+USERS_FORM;
+    echo $users;
+}
 echo "</table>";
 
 ?>
